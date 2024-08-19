7da2f21a817fed8afc41490fd205470c9df95477
henry <160744732+Hanwiiii@users.noreply.github.com>
2024-08-19 02:11:23 -0700

    simplify computer generation, add ability to play game, implement tracker for score

--- scripts.js
+++ scripts.js
@@ -1,20 +1,50 @@
-function getComputerChoice () {
-const computerChoice = Math.floor(Math.random() * 3) + 1; 
-    if (computerChoice === 1) {
-        return "Rock"; 
+let humanScore = 0;
+let computerScore = 0;
+
+// generate the number between 0-2 and assign the number to a choice of rock, paper, or scissors
+
+function getComputerChoice() {
+const computerChoice = Math.floor(Math.random() * 3); 
+    if (computerChoice === 0) {
+        return "rock"; 
+    }
+    else if (computerChoice === 1) {
+        return "paper"; 
     }
     else if (computerChoice === 2) {
-        return "Paper"; 
-    }
-    else if (computerChoice === 3) {
-        return "Scissors"; 
+        return "scissors"; 
     }
 }
-console.log(getComputerChoice());
 
-function getHumanChoice () {
-    const humanChoice = prompt("What's your choice?");
+function getHumanChoice() {
+    const humanChoice = prompt("Choose Rock, Paper, or Scissors!").toLowerCase();
     return humanChoice; 
 }
-console.log(getHumanChoice());
 
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice === computerChoice)
+        return ("Try again! It's a tie.");
+    else if (humanChoice === "rock" && computerChoice === "scissors", ++humanScore)
+        return ("You win! Rock beats Scissors!");
+    else if (humanChoice === "rock" && computerChoice === "paper", ++computerScore)
+        return ("You lose! Paper beats Rock!");
+    else if (humanChoice === "paper" && computerChoice === "rock", ++humanScore)
+        return ("You win! Paper beats Rock!");
+    else if (humanChoice === "paper" && computerChoice === "scissors", ++computerScore)
+        return ("You lose! Scissors beats Paper!");
+    else if (humanChoice === "scissors" && computerChoice === "paper", ++humanScore)
+        return ("You win! Scissors beats Paper!");
+    else if (humanChoice === "scissors" && computerChoice === "rock", ++computerScore)
+        return ("You lose! Rock beats Scissors!");
+    else {
+        return "Invalid Choice";
+    }
+}
+
+const humanSelection = getHumanChoice ();
+const computerSelection = getComputerChoice ();
+
+console.log(playRound(humanSelection, computerSelection));
+console.log(humanScore);
+console.log(computerScore);
